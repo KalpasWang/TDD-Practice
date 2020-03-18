6e8918ea536cd496c315fa57a466738d9f52a7c8
KalpasWang <wf6212@gmail.com>
2020-03-18 14:32:07 +0000

    initial version of change calculator

--- getChange.js
+++ getChange.js
@@ -1,5 +1,15 @@
 const getChange = (totalPayable, cashPaid) => {
   let change = [];
+  const availableUnits = [200, 100, 50, 20, 10, 5, 2, 1];
   
+  if(cashPaid > totalPayable) {
+    let uncountedChange = cashPaid - totalPayable;
+    let currentUnits = availableUnits;
+    while(uncountedChange > 0) {
+      currentUnits = currentUnits.filter(unit => uncountedChange - unit >= 0);
+      uncountedChange -= currentUnits[0];
+      change.push(currentUnits[0]);
+    }
+  }
   return change;
 }
--- getChange.test.js
+++ getChange.test.js
@@ -4,4 +4,18 @@ describe('getChange', () => {
   it('getChange(1,1) should equal [] - an empty array', () => {
     assert.deepEqual(getChange(1,1), []);
   });
+  
+  it('getChange(215, 300) should equal [50, 20, 10, 5]', () => {
+    assert.deepEqual(getChange(215, 300), [50, 20, 10, 5]);
+  });
+
+  it('getChange(486, 600) should equal [100, 10, 2, 2]', () => {
+    assert.deepEqual(getChange(486, 600), [100, 10, 2, 2]);
+  });
+  
+  it('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function() {
+    let result = getChange(12, 400);
+    let expected = [200, 100, 50, 20, 10, 5, 2, 1];
+    assert.deepEqual(result, expected);
+  });
 });
